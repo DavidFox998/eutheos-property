47d5759a0ff7c5603e7daa38d9e67695f1c4c889
DAVID J FOX <davidjfox998@gmail.com>
2026-08-10 20:46:04 -0700

    Remove unnecessary whitespace in lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,8 @@
 import Lake
 open Lake DSL
-
 package eutheos_property where
   leanOptions := #[⟨`autoImplicit, false⟩]
-
 require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "v4.15.0"
-
 @[default_target]
 lean_lib EutheosProperty where
   srcDir := "."
